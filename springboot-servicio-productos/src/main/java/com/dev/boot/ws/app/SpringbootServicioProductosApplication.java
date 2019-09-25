b500d392a7f2e8eb1fa56da09d6a9d3e9380905f
Alexis Gutierrez <alexisgutierrezf.1997@gmail.com>
2019-09-25 01:03:02 -0500

    Proyecto Inicial Microservicios

---
+++ springboot-servicio-productos/src/main/java/com/dev/boot/ws/app/SpringbootServicioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.dev.boot.ws.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioProductosApplication.class, args);
+	}
+
+}
